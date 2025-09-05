17b066d427549a5916c506db02b7ad47433a64b2
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-09-05 23:24:23 +0530

    with added

---
+++ src/with.rs
@@ -0,0 +1,20 @@
+
+/// Left-to-right function application (pipe-forward).
+///
+/// Equivalent to `f(a)`.
+pub fn with<A, B>(a: A, f: impl FnOnce(A) -> B) -> B {
+    f(a)
+}
+
+pub fn with_mut<A>(mut a: A, mut f: impl FnMut(&mut A)) -> A {
+    f(&mut a);
+    a
+}
+
+
+#[macro_export]
+macro_rules! with_macro {
+    ($val:expr, $f:expr) => {
+        $crate::with($val, $f)
+    };
+}
