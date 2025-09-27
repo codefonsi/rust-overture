45a2b096bc052433eb91646f05db078db4a4862d
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-09-27 13:53:55 +0530

    wip

--- examples/setter_ergonomics.rs
+++ examples/setter_ergonomics.rs
@@ -1,5 +1,4 @@
 use key_paths_derive::Keypaths;
-use overture_core::{compose::compose, pipe::pipe, with::with};
 use rust_overture::keypaths::prop;
 use std::rc::Rc;
 
@@ -22,17 +21,10 @@ fn main() {
         },
     };
     // let f = ...;
-    let f = prop(User::name_w())(Box::new(|name| name.to_uppercase()));
+    let upper_case_fn = prop(User::name_w())(Box::new(|name| name.to_uppercase()));
     // <> = backword compose
 
     // let g = ...;
-    let g = prop(Address::name_w())(Box::new(|_| String::from("new value")));
-    // let g_setter_fn = g(Box::new(|address: String| String::from(value)));
-    // forward application = |> 
-    /*
-    user
-    |> (prop(\.name)) { $0.uppercased() }
-    <> (prop(\.location.name)) { _ in "Los Angeles" }
+    let new_address_set_fn = prop(Address::name_w())(Box::new(|_| String::from("new value")));
 
-    */ 
 }
